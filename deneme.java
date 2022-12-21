0ce550366bf6d06fcfbdbd342b93423a3a3fe8c7
Alper Karabıyık <120407040+karabiyikk@users.noreply.github.com>
2022-12-21 23:51:36 +0300

    Add files via upload

---
+++ deneme.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class deneme {
+
+    static int Recursive(int base, int pow) {
+        Scanner input = new Scanner(System.in);
+        int result = 1;
+        for (int i = 1; i <= pow; i++) {
+            result *= base;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int base, pow;
+        System.out.print("Taban Değerini Giriniz : ");
+        base = input.nextInt();
+        System.out.print("Üs Değerini Giriniz : ");
+        pow = input.nextInt();
+
+        System.out.println("Sonuç : " + Recursive(base, pow));
+    }
+}
